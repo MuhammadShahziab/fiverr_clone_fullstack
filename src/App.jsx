090cb389c76475b487b7abd9cf503fcc726d7906
MuhammadShahziab <muhammadshahzaib138@gmail.com>
2024-09-09 02:27:44 +0500

    correct file name

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ import Orders from "./pages/orders/Orders";
 import Messages from "./pages/messages/Messages";
 import Message from "./pages/message/Message";
 import MyGigs from "./pages/myGigs/MyGigs";
+import Gig from "./pages/gig/Gig";
+
 import "./App.scss";
-import Gig from "./pages/gig/GIg";
 const App = () => {
   const Layout = () => {
     return (
