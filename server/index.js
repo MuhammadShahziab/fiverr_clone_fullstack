8bfd6bb1477711438c208cee8b460c1c4f172330
m_shahzaib4142 <137387477+MuhammadShahziab@users.noreply.github.com>
2024-09-27 16:26:50 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const connect = async () => {
 };
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin:[""], credentials: true }));
 app.use("/api/users", userRoute);
 app.use("/api/gigs", gigRoute);
 app.use("/api/orders", orderRoute);
